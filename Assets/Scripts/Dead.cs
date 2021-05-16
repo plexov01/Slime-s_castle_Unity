d87818cbf7bc5dcc75fc1c24a8ca2102b614ae5f
plexov.01 <plexov.01@mail.ru>
2021-05-16 20:08:54 +0700

    version 0.0.5 Add Die/thorns

--- Assets/Scripts/Dead.cs
+++ Assets/Scripts/Dead.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Dead : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform slime;
+    private Vector3 StartPos;
+
     void Start()
     {
+        StartPos=slime.transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D myTrigger)
+    {
+        if (myTrigger.CompareTag("Player"))
+        {
+            dead();
+        }
+    }
+    void dead(){
+        slime.transform.position=StartPos;
     }
 }
