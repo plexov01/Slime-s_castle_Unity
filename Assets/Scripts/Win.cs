acac3ae18407f47229ed2982c37c0ac711dc1976
plexov.01 <plexov.01@mail.ru>
2021-05-20 14:09:11 +0700

    version 0.0.6 add column/endOfLevel/latticeDoor

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    public Transform slime;
+    private Vector3 StartPos;
+
+    void Start()
+    {
+        StartPos=slime.transform.position;
+        StartPos.y+=10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D myTrigger)
+    {
+        if (myTrigger.CompareTag("Player"))
+        {
+            win();
+        }
+    }
+    void win(){
+        slime.transform.position=StartPos;
+    }
+}
